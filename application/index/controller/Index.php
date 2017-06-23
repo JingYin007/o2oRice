deda87b6570374c16398c3ad2ed47ce89b6f1c4b
moTzxx <JingYin007@users.noreply.github.com>
2017-06-23 16:23:32 +0800

    log

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -2,12 +2,14 @@
 namespace app\index\controller;
 
 use think\Controller;
+use think\Log;
 
 class Index extends Controller
 {
     public function index()
     {
-
+        Log::write('o2o_JJJJJ','log');
+        trace('o2o-trace','log');
         return $this->fetch();
     }
 }
